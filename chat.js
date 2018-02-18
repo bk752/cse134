7d3ea9d0beb0c9b97c2b6f2b025f3aaaa472bcc5
benkphan <bkp004@ucsd.edu>
2018-02-18 01:18:59 -0800

    almost done with chat

--- chat.js
+++ chat.js
@@ -1,7 +1,8 @@
 class Message {
-	constructor(owner, message) {
+	constructor(owner, message, part) {
 		this.owner = owner;
-		this.message = message;
+        this.message = message;
+        this.part = part;
 	}
 }
 
@@ -9,7 +10,23 @@ let messages = [
     new Message("user", "Hello I would like some help choosing parts."),
     new Message("expert", "Hello. What can I help you with?"),
     new Message("user", "I am not sure what kind of motherboard I should get."),
-    new Message("user", "Do you have any recommendations?")
+    new Message("user", "Do you have any recommendations?"),
+    new Message("expert", "How does this motherboard look?", new Part("ATX Motherboard", "Great Motherboard for $150", "images/mb1.jpeg"))
+];
+
+let parts = [
+	new Category("CPU").addPart([
+		new Part("i5 Processor", "$197.89", "images/i5.jpeg"),
+		new Part("i7 Processor", "$569.00", "images/i7.jpeg"),
+	]),
+	new Category("Motherboard").addPart([
+		new Part("Micro ATX Motherboard", "$59.99", "images/mb1.jpeg"),
+		new Part("ATX Motherboard", "$109.99", "images/mb2.jpeg"),
+	]),
+	new Category("GPU").addPart([
+		new Part("NVIDIA GTX 1060", "$489.89", "images/gpu1.jpeg"),
+		new Part("Radeon RX 570", "$636.99", "images/gpu3.jpeg"),
+	]),
 ];
 
 function addMessage(message) {
@@ -20,12 +37,44 @@ function addMessage(message) {
     msg.innerHTML = message.message;
     holder.appendChild(msg);
     chatLog.appendChild(holder);
+    if (typeof(message.part) != "undefined") {
+        let partEle = document.createElement("div");
+        partEle.setAttribute("class", "part-card");
+        
+        let imageEle = document.createElement("img");
+        imageEle.setAttribute("class", "part-card__image");
+        imageEle.setAttribute("src", message.part.image);
+        imageEle.setAttribute("alt", "placeholder");
+
+        let title = document.createElement("div");
+        title.setAttribute("class", "part-card__title");
+        title.innerHTML = message.part.name;
+
+        let info = document.createElement("div");
+        info.setAttribute("class", "part-card__description");
+        info.innerHTML = message.part.desc;
+
+        let button = document.createElement("button");
+        button.setAttribute("class", "part-card__add");
+        button.innerHTML = "Add item to build";
+
+        msg.appendChild(partEle);
+        partEle.appendChild(imageEle);
+        partEle.appendChild(title);
+        partEle.appendChild(info);
+        partEle.appendChild(button);
+    }
 }
 let chatLog = document.getElementById("chatLog");
 let messageBox = document.getElementById("chatTextbox");
 messages.forEach((message, index)=> {
     addMessage(message);
 })
+
+function closeParts() {
+    let partsMessageHolder = document.getElementById("partsMessageHolder");
+    chatLog.removeChild(partsMessageHolder);
+}
 
 function sendOnClick() {
     sendMessage();
@@ -42,13 +91,72 @@ function sendMessage() {
     chatLog.scrollTop = chatLog.scrollHeight;
 
 }
+function displayParts(cat_name) {
+    
 
+    for (j = 0; j < parts.length; j++) {
+        let cat = parts[j];
+        if (cat.name.toLowerCase() === cat_name) {
+            let partsList = cat.parts;
+            let holder = document.createElement("div");
+            holder.setAttribute("class", "chat__message-holder chat__message-holder--user");
+            holder.setAttribute("id", "partsMessageHolder");
+            let msg = document.createElement("div");
+            msg.setAttribute("class", "chat__message");
+            holder.appendChild(msg);
+            chatLog.appendChild(holder);
+            for (i = 0; i < partsList.length; i++) {
+                let part = partsList[i];
+                let partEle = document.createElement("div");
+                partEle.setAttribute("class", "part-card");
+                
+                let imageEle = document.createElement("img");
+                imageEle.setAttribute("class", "part-card__image");
+                imageEle.setAttribute("src", part.image);
+                imageEle.setAttribute("alt", "placeholder");
+
+                let title = document.createElement("div");
+                title.setAttribute("class", "part-card__title");
+                title.innerHTML = part.name;
+
+                let info = document.createElement("div");
+                info.setAttribute("class", "part-card__description");
+                info.innerHTML = part.desc;
+
+                partEle.appendChild(imageEle);
+                partEle.appendChild(title);
+                partEle.appendChild(info);
+                msg.appendChild(partEle);
+            }
+            let button = document.createElement("button");
+            button.setAttribute("class", "part-card__add");
+            button.setAttribute("onclick", "closeParts()");
+            button.innerHTML = "Close";
+            msg.appendChild(button);
+        }
+    }
+    chatLog.scrollTop = chatLog.scrollHeight;
+}
+
+function partsOnClick() {
+    let text = messageBox.value.toLowerCase();
+    if (text==="") {
+        return;
+    }
+    messageBox.value = "";
+    displayParts(text);
+}
 function keyPress(e) {
-    var key = e.keyCode;
+    let key = e.keyCode;
 
     // If the user has pressed enter
     if (key === 13) {
-        sendMessage();
-		e.preventDefault();
+        if (e.ctrlKey) {
+            partsOnClick();
+        }
+        else {sendMessage();
+            e.preventDefault();
+        }
+        
     }
 }
