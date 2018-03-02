bab871013066fd2d15204d6fa0f17ccd2e2e761e
brian <bk752@github.com>
2018-03-02 15:52:32 -0800

    Revert switch srcServer to webpack-dev-server

--- tools/srcServer.js
+++ tools/srcServer.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import webpack from 'webpack';
-import WebpackDevServer from 'webpack-dev-server';
 import path from 'path';
 import config from '../webpack.config.dev';
 import open from 'open';
@@ -8,21 +7,24 @@ import open from 'open';
 /* eslint-disable no-console */
 
 const port = 3000;
-console.log('Starting the dev web server...');
+const app = express();
+const compiler = webpack(config);
 
-const options = {
-  publicPath: config.output.publicPath,
-  hot: true,
-  inline: true,
-  contentBase: './src',
-  stats: { colors: true }
-};
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
 
-const server = new WebpackDevServer(webpack(config), options);
+app.use(require('webpack-hot-middleware')(compiler));
 
-server.listen(port, 'localhost', function (err) {
+app.get('*', function(req, res) {
+  res.sendFile(path.join( __dirname, '../src/index.html'));
+});
+
+app.listen(port, function(err) {
   if (err) {
     console.log(err);
+  } else {
+    open(`http://localhost:${port}`);
   }
-  console.log('WebpackDevServer listening at localhost:', port);
 });
