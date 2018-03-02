3d67d40819057e636ad8d57d9b8c2b504c925e08
brian <bk752@github.com>
2018-03-02 13:49:11 -0800

    Write parts tests

---
+++ src/components/common/PartsOption.test.js
@@ -0,0 +1,60 @@
+import expect from 'expect';
+import React from 'react';
+import ShallowRenderer from 'react-test-renderer/shallow';
+import PartsCategory from './PartsCategory';
+import Category from '../../objects/Category';
+import Part from '../../objects/Part';
+import Timeline from '../common/Timeline';
+
+function setup(saving) {
+  let props = {
+	  category: new Category("Category"),
+	  first: true,
+	  last: true,
+	  active: true,
+  };
+
+  let renderer = new ShallowRenderer();
+  renderer.render(<PartsCategory {...props}/>);
+  let output = renderer.getRenderOutput();
+
+  return {
+    props,
+    output,
+    renderer
+  };
+}
+
+describe('PartsCategory via React Test Utils', () => {
+  it('renders form in Timeline', () => {
+    const { output } = setup();
+    expect(output.type).toEqual(Timeline);
+    let form = output.props.children;
+    expect(form.type).toBe('form');
+  });
+});
+
+
+/*
+import React from 'react';
+import expect from 'expect';
+import {mount, shallow} from 'enzyme';
+import {ManageCoursePage} from './ManageCoursePage';
+
+describe ('Manage Course Page', () => {
+  it('sets error message when trying to save empty title', () => {
+    const props = {
+      authors: [],
+      actions: { saveCourse: () => { return Promise.resolve(); }},
+      course: {id: '', watchHref: '', title: '', authorId: '', length: '', category: ''}
+    };
+
+    const wrapper = mount(<ManageCoursePage {...props}/>);
+    const saveButton = wrapper.find('input').last();
+    expect(saveButton.prop('type')).toBe('submit');
+    saveButton.simulate('click');
+    expect(wrapper.state().errors.title).toBe('Title must be at least 5 characters.');
+
+  });
+});
+ */
---
+++ src/components/parts/PartsCategory.test.js
@@ -0,0 +1,55 @@
+import expect from 'expect';
+import React from 'react';
+import { mount, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import ShallowRenderer from 'react-test-renderer/shallow';
+import PartsCategory from './PartsCategory';
+import Category from '../../objects/Category';
+import Part from '../../objects/Part';
+import Timeline from '../common/Timeline';
+
+
+function setup(saving) {
+  let props = {
+	  category: new Category("Category"),
+	  first: true,
+	  last: true,
+	  active: true,
+  };
+
+  let renderer = new ShallowRenderer();
+  renderer.render(<PartsCategory {...props}/>);
+  let output = renderer.getRenderOutput();
+
+  return {
+    props,
+    output,
+    renderer
+  };
+}
+
+describe('PartsCategory via React Test Utils', () => {
+  configure({ adapter: new Adapter() });
+  it('renders form in Timeline', () => {
+    const { output } = setup();
+    expect(output.type).toEqual(Timeline);
+    let form = output.props.children;
+    expect(form.type).toBe('div');
+  });
+
+  it('sets error message when trying to save empty title', () => {
+	let index = 0;
+    let props = {
+        category: new Category("Category"),
+        first: true,
+        last: true,
+        active: true,
+        prevCategory: () => index = index - 1
+    };
+    const wrapper = mount(<PartsCategory {...props}/>);
+    const prevButton = wrapper.find('button').last();
+    expect(prevButton.prop('onClick')).toEqual(props.prevCategory);
+    prevButton.simulate('click');
+    expect(index).toBe(-1);
+  });
+});
