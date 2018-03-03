872a5f759e1018bc5fbd8fe1541bbc58e01da69a
brian <bk752@github.com>
2018-03-02 18:23:03 -0800

    Fix dev server not finding bundle.js on Windows

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -11,7 +11,7 @@ export default {
   ],
   target: 'web',
   output: {
-    path: __dirname + '/dist', // Note: Physical files are only output by the production build task `npm run build`.
+    path: '/', // Note: Physical files are only output by the production build task `npm run build`.
     publicPath: '/',
     filename: 'bundle.js'
   },
