f0f7eac884f7903cf0b1a2b0762265f28fc7db0a
brian <bk752@github.com>
2018-03-12 19:08:27 -0700

    Fix parts page test

--- src/components/parts/PartsPage.test.js
+++ src/components/parts/PartsPage.test.js
@@ -1,24 +1,20 @@
 import expect from 'expect';
 import React from 'react';
-import { mount, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import ShallowRenderer from 'react-test-renderer/shallow';
-import PartsCategory from './PartsCategory';
-import Category from '../../objects/Category';
-import Part from '../../objects/Part';
-import Timeline from '../common/Timeline';
+import {PartsPage} from './PartsPage';
+import CenterPage from '../common/CenterPage';
+import initialState from '../../reducers/initialState';
 
 
 function setup(saving) {
 	let props = {
-		category: new Category("Category"),
-		first: true,
-		last: true,
-		active: true,
+		parts: initialState.parts.list,
+		active: initialState.parts.active
 	};
 
 	let renderer = new ShallowRenderer();
-	renderer.render(<PartsCategory {...props}/>);
+	renderer.render(<PartsPage {...props}/>);
 	let output = renderer.getRenderOutput();
 
 	return {
@@ -29,39 +25,11 @@ function setup(saving) {
 }
 
 describe('PartsCategory via React Test Utils', () => {
-	let index = 0;
-	configure({ adapter: new Adapter() });
-	let props = {
-		category: new Category("Category"),
-		first: true,
-		last: true,
-		active: true,
-		prevCategory: () => index = index - 1,
-		nextCategory: () => index = index + 1
-	};
-
-	it('renders form in Timeline', () => {
+	it('renders div in CenterPage', () => {
 		const { output } = setup();
-		expect(output.type).toEqual(Timeline);
-		let form = output.props.children;
-		expect(form.type).toBe('div');
-	});
-
-	it('Check next section button', () => {
-		index = 0;
-		const wrapper = mount(<PartsCategory {...props}/>);
-		const prevButton = wrapper.find('button').last();
-		expect(prevButton.prop('onClick')).toEqual(props.prevCategory);
-		prevButton.simulate('click');
-		expect(index).toBe(-1);
-	});
-
-	it('Check previous section button', () => {
-		index = 0;
-		const wrapper = mount(<PartsCategory {...props}/>);
-		const nextButton = wrapper.find('button').at(2);
-		expect(nextButton.prop('onClick')).toEqual(props.nextCategory);
-		nextButton.simulate('click');
-		expect(index).toBe(1);
+		expect(output.type).toEqual(CenterPage);
+		let div = output.props.children;
+		expect(div.type).toBe('div');
+		expect(div.props.children.length).toBe(initialState.parts.list.length);
 	});
 });
