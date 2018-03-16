27a6d326143f7fc8e3294b624d80457c5b19b766
benkphan <bkp004@ucsd.edu>
2018-03-16 05:04:00 -0700

    fix store test

--- src/store/store.test.js
+++ src/store/store.test.js
@@ -1,24 +1,32 @@
 import expect from 'expect';
 import { createStore } from 'redux';
 import rootReducer from '../reducers';
-import initialState from '../reducers/initialState';
+//import initialState from '../reducers/initialState';
+import {totalParts} from '../api/partsApi';
 import * as partsActions from '../actions/partsActions';
 import Part from '../objects/Part';
 
 describe('Store', function() {
 	it('Should handle creating courses', function() {
 		// arrange
+		let initialState = {
+			parts: {
+				list: totalParts,
+				active: 0,
+				adding: 0
+			}
+		};
 		const store = createStore(rootReducer, initialState);
-		const category = initialState.parts.list[0];
+		const category = totalParts[0];
 
 		// act
 		const action = partsActions.addPart("part", "40.00", "img", category);
 		store.dispatch(action);
 
 		// assert
-		//const actual = store.getState().parts.list[0].parts[3];
+		const actual = store.getState().parts.list[0].parts[3];
 		const expected = new Part("part", 40.00, "img");
 
-		//expect(actual).toEqual(expected);
+		expect(actual).toEqual(expected);
 	});
 });
